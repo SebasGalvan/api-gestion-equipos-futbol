1919858d108c2eec09abfb3028ab58faf66e276c
Sebastian Galvan <sebastiangalvan.ar@gmail.com>
2024-09-17 14:23:36 -0300

    Main

---
+++ src/main/java/com/dux/api/equipos/apiequiposfutbol/ApiEquiposFutbolApplication.java
@@ -0,0 +1,13 @@
+package com.dux.api.equipos.apiequiposfutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEquiposFutbolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiEquiposFutbolApplication.class, args);
+    }
+
+}
